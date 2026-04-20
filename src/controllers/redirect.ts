ae96a23ac28c3ef63a8c0a5a928e32e52dad21f7
axtk <axtkx@inbox.ru>
2026-04-20 18:26:46 +0300

    add log to redirect controller

--- src/controllers/redirect.ts
+++ src/controllers/redirect.ts
@@ -1,4 +1,5 @@
 import type { Controller } from "../types/Controller.ts";
+import { emitLog } from "../utils/emitLog.ts";
 
 export type RedirectParams = {
   url: string;
@@ -13,6 +14,15 @@ export const redirect: Controller<string | RedirectParams> = (params) => {
     let search = req.originalUrl.split("?")[1] ?? "";
 
     if (search) search = `${url.includes("?") ? "&" : "?"}${search}`;
+
+    emitLog(req.app, "Redirecting", {
+      data: {
+        from: req.originalUrl,
+        to: `${url}${search}`,
+      },
+      req,
+      res,
+    });
 
     res.redirect(status, `${url}${search}`);
   };
